3788d8563b0443af05f6254a404a9abe00387358
Rohit442 <rohitrajaa04@gmail.com>
2023-03-20 13:32:04 +0530

    package and DriverDashboard

--- screens/DriverDashboard.js
+++ screens/DriverDashboard.js
@@ -1,10 +1,12 @@
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
+import React, { useState } from "react";
+import { Calendar, LocaleConfig } from "react-native-calendars";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { auth } from "../firebase";
 
 const DriverDashboard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState("");
 
   const handleSignOut = () => {
     auth
@@ -16,11 +18,45 @@ const DriverDashboard = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <View style={styles.SignOutButtonContainer}>
+        <TouchableOpacity onPress={handleSignOut} style={styles.SignOutButton}>
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <Calendar
+          style={{
+            borderWidth: 1,
+            borderColor: "gray",
+            height: 370,
+            borderRadius: 15,
+            position: "relative",
+            bottom: 10,
+            width: 300,
+          }}
+          markedDates={{
+            "2023-03-01": {
+              selected: true,
+              marked: true,
+              selectedColor: "blue",
+            },
+            "2023-03-02": { marked: true },
+            "2023-03-03": {
+              selected: true,
+              marked: true,
+              selectedColor: "blue",
+            },
+          }}
+          onDayPress={(day) => {
+            setSelected(day.dateString);
+          }}
+        />
+        <Text>Email: {auth.currentUser?.email}</Text>
+        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -40,6 +76,19 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: "center",
     marginTop: 40,
+  },
+
+  SignOutButton: {
+    backgroundColor: "blue",
+    padding: 5,
+    borderRadius: 5,
+    margin: 2,
+  },
+  SignOutButtonContainer: {
+    position: "absolute",
+    right: 0,
+    top: 5,
+    flexDirection: "row",
   },
   buttonText: {
     color: "white",
